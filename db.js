39808dab53d214eb4a236cd1d6f2223a2967ab7e
Didouu <accbasic2519@gmail.com>
2026-02-12 19:25:28 +0000

    variables

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"251925",
-    database:"ramadhan",
+    host: process.env.MYSQLHOST,
+    user:process.env.MYSQLUSER,
+    password:process.env.MYSQLPASSWORD,
+    database:process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
 });
 
 export default db ;
