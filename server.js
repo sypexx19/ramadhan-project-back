36846cf3fa00004acb5322e0ae506249dff52473
Didouu <accbasic2519@gmail.com>
2026-02-13 16:19:10 +0000

    server edit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen ( PORT , () => { console.log( `server running on port ${PORT}`)});
