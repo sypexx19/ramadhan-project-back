e1e1adacb611ba6176a41c76b46691b576945965
Didouu <accbasic2519@gmail.com>
2026-02-13 16:02:33 +0000

    server edit

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen ( PORT , () => { console.log( `server running on port ${PORT}`)});
